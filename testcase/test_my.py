e5b11d88860d8d4a071dfa708e1c638c86266c95
“1213adam” <171639931@qq.com>
2020-09-07 16:41:05 +0800

    add new files

---
+++ testcase/test_my.py
@@ -0,0 +1,10 @@
+def test_001():
+    print(" testing complet11")
+
+
+def test_002():
+    print(" testing complete2")
+
+
+def test_003():
+    print(" testing complete3")
---
+++ testsuite/suite1.robot
@@ -0,0 +1,12 @@
+*** Settings ***
+
+Library     testcase.test_my
+
+*** Test Case ***
+
+MY_001
+    test_001
+MY_002
+    test_002
+MY_003
+    test_003
